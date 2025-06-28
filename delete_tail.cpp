811e3542ba7535342d23509c611eb6fe1f2b2d4c
Nishant Mishra <nishantmishra3108@gmail.com>
2025-06-29 00:37:14 +0530

    Added Files and code changes

---
+++ delete_tail.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+    int data;
+    node* next;
+    public:
+    node(int data1, node* next1 = nullptr) {
+        data = data1;
+        next = next1;
+    }
+};
+
+node* convert2LL(vector<int> &arr){
+    node* head = new node(arr[0]);
+    node* mover = head;
+    for(int i = 1; i < arr.size(); i++){
+        node* temp = new node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void printLL(node* head){
+    node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+node* deleteTail(node* head){
+    if (head == nullptr || head->next == nullptr) return nullptr;
+    node* temp = head;
+    while(temp->next->next != nullptr){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = nullptr;
+    return head;
+}
+
+int main() {
+    int n;
+    cin  >> n;
+    vector <int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    node* head = convert2LL(arr);
+
+    printLL(head);
+
+    head = deleteTail(head);
+    cout << endl;
+
+    printLL(head);
+    return 0;
+}
