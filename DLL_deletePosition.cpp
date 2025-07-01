ac831845badedfd5f3de0ac1778f387375d49f89
Nishant Mishra <nishantmishra3108@gmail.com>
2025-07-01 23:53:13 +0530

    Added Files and code changes

--- DLL_deletePosition.cpp
+++ DLL_deletePosition.cpp
@@ -1,22 +1,21 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// deleting the last node of the DLL, aka tail
-
-class node {
+class node{
     public:
     node* prev;
     int data;
     node* next;
+
     public:
-    node(int data1, node* next1 = nullptr, node* prev1 = nullptr) {
+    node(int data1, node* next1 = nullptr, node* prev1 = nullptr){
         prev = prev1;
         data = data1;
         next = next1;
     }
 };
 
-node* convert2LL(vector<int> &arr){
+node* convert2LL(vector <int> arr){
     node* head = new node(arr[0]);
     node* prev = head;
     for(int i = 1; i < arr.size(); i++){
@@ -35,37 +34,48 @@ void printLL(node* head){
     }
 }
 
-node* DLL_deleteTail(node* head){
+node* DLL_deletePos(node* head, int k){
     if (head == nullptr) return nullptr;
-    
-    if (head->next == nullptr) {
-        delete head;
-        return nullptr;
+    if (k == 1) {
+        node* temp = head;
+        head = head->next;
+        if (head) head->prev = nullptr;
+        delete temp;
+        return head;
     }
+
     node* temp = head;
-    while(temp->next != nullptr){
+    int cnt = 1;
+    while(temp && cnt < k){
         temp = temp->next;
+        cnt++;
     }
+    
+    if(!temp) return head;
+
     node* prev = temp->prev;
-    prev->next = nullptr;
-    temp->prev = nullptr;
+    node* next = temp->next;
+
+    if (prev) prev->next = next;
+    if (next) next->prev = prev;
+
     delete temp;
     return head;
 }
 
-int main() {
+int main(){
     int n;
-    cin  >> n;
-    vector <int> arr(n);
+    cin >> n;
+    vector <int>arr(n);
     for(int i = 0; i < n; i++) cin >> arr[i];
-
+    // call
     node* head = convert2LL(arr);
-
     printLL(head);
-
-    head = DLL_deleteTail(head);
     cout << endl;
 
+    int k;
+    cin >> k;
+    head = DLL_deletePos(head, k);
     printLL(head);
     return 0;
 }
