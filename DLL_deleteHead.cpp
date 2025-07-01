09109cd6b785457f6ff5fe6ba37dc508aaf48ff6
Nishant Mishra <nishantmishra3108@gmail.com>
2025-07-01 22:50:54 +0530

    Added Files and code changes

---
+++ DLL_deleteHead.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// deleting the first node of the LL, aka head
+
+class node {
+    public:
+    node* prev;
+    int data;
+    node* next;
+    public:
+    node(int data1, node* next1 = nullptr, node* prev1 = nullptr) {
+        prev = prev1;
+        data = data1;
+        next = next1;
+    }
+};
+
+node* convert2LL(vector<int> &arr){
+    node* head = new node(arr[0]);
+    node* prev = head;
+    for(int i = 1; i < arr.size(); i++){
+        node* temp = new node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+void printLL(node* head){
+    node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+node* deleteHead(node* head){
+    if (head == nullptr) return nullptr;
+    node* prev = head;
+    head = head->next;
+    head->prev = nullptr;
+    prev->next = nullptr;
+    return head;
+}
+
+int main() {
+    int n;
+    cin  >> n;
+    vector <int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    node* head = convert2LL(arr);
+
+    printLL(head);
+
+    head = deleteHead(head);
+    cout << endl;
+
+    printLL(head);
+    return 0;
+}
