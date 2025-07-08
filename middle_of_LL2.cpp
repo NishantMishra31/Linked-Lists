2783af046abc609c6090542bc105e8465be523e5
Nishant Mishra <mnishant.work@gmail.com>
2025-07-08 21:02:52 +0530

    Added Files and code changes

---
+++ middle_of_LL2.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    public:
+    node(int data1, node* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+node* convert2LL(vector <int> arr){
+    int n = arr.size();
+    node* head = new node(arr[0], nullptr);
+    node* mover = head;
+    for(int i = 1; i < n; i++){
+        node* temp = new node(arr[i], nullptr);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void printLL(node* head){
+    node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+node* middleofLL(node* head){
+    node* slow = head;
+    node* fast = head;
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    // call
+    node* head = convert2LL(arr);
+    printLL(head);
+
+    cout << endl << middleofLL(head);
+    return 0;
+}
