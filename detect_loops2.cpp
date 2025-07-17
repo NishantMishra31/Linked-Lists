d64efef657beb646378813b3d83a86952a55103d
Nishant Mishra <mnishant.work@gmail.com>
2025-07-17 20:18:36 +0530

    Added Files and code changes

---
+++ detect_loops2.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    public:
+    node(int data1, node* next1 = nullptr){
+        data = data1;
+        next = next1;
+    }
+};
+
+node* convert2LL(vector <int> arr){
+    int n = arr.size();
+    node* head = new node(arr[0]);
+    node* mover = head;
+    for(int i = 1; i < n; i++){
+        node* temp = new node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void printLL(node* head){
+    node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+bool hasLoops(node* head){
+    if (!head || head->next == nullptr) return false;
+    node* slow = head;
+    node* fast = head->next->next;
+    while(fast != nullptr && fast->next != nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    // call
+    node* head = convert2LL(arr);
+    printLL(head);
+
+    // test this code with loops in the LL
+    cout << endl << (hasLoops(head) ? "true" : "false");
+    return 0;
+}
