a2692a79186aa5782d2c8378c7cf2dd6dda21e2b
Nishant Mishra <mnishant.work@gmail.com>
2025-07-20 23:02:44 +0530

    Added Files and code changes

---
+++ intersection_LL.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node(int data1, node* next1 = nullptr){
+        data = data1;
+        next = next1;
+    }
+};
+
+node* convert2LL(vector <int> arr){
+    int n = arr.size();
+    node* head = new node(arr[0]);
+    node* mover = head;
+    for(int i = 1; i < n; i++){
+        node* temp = new node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void printLL(node* head){
+    node* temp = head;
+    while(temp){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    // call
+    node* head = convert2LL(arr);
+    printLL(head);
+    return 0;
+}
